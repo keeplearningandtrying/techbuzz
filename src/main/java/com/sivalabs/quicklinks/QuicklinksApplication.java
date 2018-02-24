013dec08dd40a651535d664a76810e119f97a897
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2018-02-24 08:51:24 +0530

    Init.

---
+++ src/main/java/com/sivalabs/quicklinks/QuicklinksApplication.java
@@ -0,0 +1,12 @@
+package com.sivalabs.quicklinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicklinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicklinksApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/data.js
@@ -0,0 +1,13 @@
+var data = [
+    {
+        "title" : "Session Management using Spring Session with JDBC DataStore",
+        "url": "https://sivalabs.in/2018/02/session-management-using-spring-session-jdbc-datastore/",
+        "tags": ["java","spring","spring-boot", "spring-session"]
+    },
+    {
+        "title" : "CI/CD for SpringBoot applications using Travis-CI",
+        "url": "https://sivalabs.in/2018/01/ci-cd-springboot-applications-using-travis-ci/",
+        "tags": ["java","travis-ci","spring-boot"]
+    }
+
+]
