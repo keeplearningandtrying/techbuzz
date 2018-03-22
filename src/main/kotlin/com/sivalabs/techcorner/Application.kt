5fefc9a7d970683ff322477425198353d52b63bc
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2018-03-22 14:57:41 +0530

    Adding kubernetes config files.

--- src/main/kotlin/com/sivalabs/techcorner/Application.kt
+++ src/main/kotlin/com/sivalabs/techcorner/Application.kt
@@ -1,10 +1,22 @@
 package com.sivalabs.techcorner
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Application
+class Application {
+
+    @Bean
+    fun commonTags(): MeterRegistryCustomizer<MeterRegistry> {
+        return MeterRegistryCustomizer { registry ->
+                registry.config().commonTags("application", "techcorner")
+        }
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
